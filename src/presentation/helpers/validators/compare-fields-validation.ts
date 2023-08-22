4f5a8c3b587a1a879d748e43841cee5f45bc5097
Douglas Baltazar <douglasbaltazar1@gmail.com>
2023-08-22 10:38:10 -0300

    refactor: move EmailValidation to the validationComposite

--- src/presentation/helpers/validators/compare-fields-validation.ts
+++ src/presentation/helpers/validators/compare-fields-validation.ts
@@ -10,7 +10,7 @@ export class CompareFieldsValidation implements Validation {
   }
 
   validate (input: any): Error | null {
-    if (!input[this.fieldName] !== input[this.fieldToCompareName]) {
+    if (input[this.fieldName] !== input[this.fieldToCompareName]) {
       return new InvalidParamError(this.fieldToCompareName)
     }
     return null
